7718d45c356c566960ddc11d8d0ab9542f603d35
JenifferGenoves <jenifer.genoves03@gmail.com>
2020-05-13 11:22:50 -0400

    avance

--- src/data.js
+++ src/data.js
@@ -1,22 +1,96 @@
-//Tenemos que comunicar la función que creamos en el archivo main,con la función que crearemos en este archivo Data
-//Creamos una función que va a recibir lo que viene del main.js
-//Desde Main se llama a la función y se entrega los 2 parámetros
-//La condición es lo que el usuario selecciona en el HTML
-//ACA SE DEBE PONER LA FUNCIÓN PARA FILTRAR
-export const filterData = (data, condition) => {
+// En esta sección van las funciones de manipulación de la Data
+//Primero scribimos la función para mostrar a todos los Pokemones
+
+export const filterData = (pokeData, condition) => {
+    //console.log(data);
+    //console.log(condition);
+    //Le vamos a pedir que recorra la Data, la data es un ARRAY que contiene Objetos, para eso utilizamos: For hasta el largo de la data y uno por
+    
+    
+      }
+
+
+
+
+
+
+//   //console.log(condition);
+//   //Le vamos a pedir que recorra la Data, la data es un ARRAY que contiene Objetos, para eso utilizamos: For hasta el largo de la data y uno por
+  
+//   const pokeData = [];
+  
+//   for (let i = 0; i < pokeData.length; i++) {
+//       pokeData.push(pokeData[i].name);
+
+//   }
+// // // export const allPokemon = (dataSet) => {
+// //       const newDataArray = [];
+// //       for (let i = 0; i < dataSet.length; i += 1) {
+// //         newDataArray.push(dataSet[i]);
+// //       }
+// //       return newDataArray;
+// //     };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //console.log(data);
 //console.log(condition);
 //Le vamos a pedir que recorra la Data, la data es un ARRAY que contiene Objetos, para eso utilizamos: For hasta el largo de la data y uno por
 
-let names = [];
-for (let i = 0; i < data.length; i++) {
-  names.push(data[i].name)
-  }
 
-return names;
-//console.log(names)
-  //console.log(data[i].name) //Nos muestra cada objeto que está en las distintas posiciones del arreglo. Quiero pedirle que solo me muestre los nombres con .name
- //Como tenemos esto, ahora lo mandamos al Dom(main), para que haga la estructura y se lo pase al HTML
-//Para lo anterior guardamos la información en una variable, creamos una nueva variable(arriba de for)
- 
-}
+
+
+// //Ordena Pokemones por nombre de la A a la Z
+// export const orderPokemonAz = (dataSet)=> {
+//   const newDataArray = [];
+//   for (let i = 0; i < dataSet.length; i++) {
+//     newDataArray.push(dataSet[i]);
+//   }
+//   newDataArray.sort((a, b) => {
+//     if (a.name > b.name) {
+//       return 1;
+//     }
+//     if (a.name < b.name) {
+//       return -1;
+//     }
+//     return 0;
+//   });
+//   return newDataArray;
+// };
+
+// //Ordena Pokemones por nombre de la Z a la A
+// export const orderPokemonZa = (dataSet)=>{
+//   const newDataArray = [];
+//   for (let i = 0; i < dataSet.length; i ++) {
+//     newDataArray.push(dataSet[i]);
+//   }
+//   newDataArray.sort((a, b) => {
+//     if (a.name > b.name) {
+//       return -1;
+//     }
+//     if (a.name < b.name) {
+//       return 1;
+//     }
+//     return 0;
+//   });
+//   return newDataArray;
+// };
--- src/main.js
+++ src/main.js
@@ -1,56 +1,378 @@
-//Aqui utilizamos todo lo que es manipulación del DOM.
-import data from './data/pokemon/pokemon.js';
-import { filterData } from './../src/data.js';
+// EN ESTA SECCIÓN VARIABLES PARA MANIPULAR EL DOM
+// DENTRO DE LAS LLAVES DE CADA FUNCIÓN ESCRIBO LAS FUNCIONES QUE DECLARO EN EL DATA.
+
+import pokeData from './data/pokemon/pokemon.js';
+
+import { filterData } from './data.js';
+
+//MOSTRAR A POKEMONES EN POKEDEX. 
+//Método Object.values() devuelve un array con los valores correspondientes a las propiedades enumerables de un objeto.
+const pokeDatos = pokeData.pokemon;
+let arrData = Object.values(pokeDatos)
+const pokemonesObj = document.getElementById("containerPokemon");
+for (let i = 0; i < arrData.length; i++) {
+  pokemonesObj.innerHTML += `<div class="cardP">
+    <h3 class="pokeNumber">${arrData[i].num}</h3>
+    <img src="${arrData[i].img}" alt="" class="pokeImg">
+    <h4 class="pokeName">${arrData[i].name}</h4>
+  </div>`;
+  // createModal();
+}
 
 
-//Creamos una variable para guadar los tipos de Pokemon
-const selector = document.getElementById('selectorType');
-const container = document.getElementById('root');
+//RANKING-TOP 10
+const pokeDatos2 = pokeData.pokemon;
+let arrData2 = Object.values(pokeDatos2)
+const pokemonesObj2 = document.getElementById("tableOne");  
+for (let i = 0; i <9; arrData.length; i++) {
+  pokemonesObj2.innerHTML += `<div class="cardP">
+    <h3 class="pokeNumber">${arrData2[i].num}</h3>
+    <img src="${arrData2[i].img}" alt="" class="pokeImg2">
+    <h4 class="pokeName">${arrData2[i].name}</h4>
+  </div>`;
 
-// Creamos un evento para sobre la cual se ejecutará la variable anterior. AddEventlistener esta compuesto de 2 cosas 
-// (el evento, el nombre de la función a la cual le daremos ese evento) o podemos dejar la función anónima y poner todo dentro del evento.
-// Con la variable let condition; guardamos una y otra vez el valor que el usuario seleccione de este select
-// Esto es importante para decirle a la Data, muestrame solo los Pokemon de algún tipo (TENEMOS  QUE HACERLO)
+//ORDENAR A LOS POKEMONES DESDE LA A-Z
 
-selector.addEventListener('change', () => {
-    let condition = selector.options[selector.selectedIndex].text; 
-    //console.log(condition)
-    let result = filterData(data, condition);
-   // console.log(result)
-    for(let i= 0; i < result.length; i++){
-      //Aqui armamos la estructura del HTML,Con el InerHTML le inyecto cosas a una estructura HTML
-      //container.innerHTML += '<p>'+ result[i] +'</p>';
-      container.innerHTML += `<p>${result[i]}</p>`
-    }
-    
+const selectOrder = document.getElementById("filterOrder");
+selectOrder.addEventListener("change", ()=>{
+  let selectedOrder = filterData(selectOrder);
 
+  document.getElementById("containerPokemon").innerHTML="";
 })
-//creo una variable que se va a llamar pokeDat y dentro guardará data.pokemon que es lo que estoy importando
-let pokeDat = data.pokemon;
-//Ahora pido un console.log de la variable pokeDat que acabo de crear y es esta variable con la cual yo trabajo, con 
-//La variable porque aquí esta guardado todo el arreglo de objetos
-//CON LA DATA DE POKEMOM VAMOS A HACER 1 PASO MAS PORQUE NO VIENE LISTO EL ARREGLO, TENEMOS QUE HACER EL ARREGLO PARA CREAR UNA VARIABLE Y GUARDAR ESE OBJETO.
-console.log(pokeDat)
-
-// Hay una propiedad en Js que se llama Objet y es para transformar un valor de un objeto en arreglos
-//console.log(Object.values(lolData))
 
 
-// **si en result no se Hubiera guardado un arreglo con nombres, sino un arreglo de objetos, como le pido que me muestre un nombre
-//  container.innerHTML += `<p>${result[i].name}</p>`
 
 
-//Si queremos importar toda la data y no condición por condición, se pone al final del archivo con un export (estilo objeto)
-//Y se exporta todas las funciones (Buscar en google)
 
 
-//let dataArray = object.values(data.data);
-//console.log(dataArray)
 
-//const input = document.getElementById('input-name');
-//const btnName = document.getElementById('btn-name');
-//const container =document.getElementById('root');
-//btnName.addEventListener('click', () => {
-  //let name = inputName.value;
-   //let resultConfirmName = confirmName(dataArray, name);})
-   
+
+
+
+/*
+//seleccionar ordenar por tipo
+const selectType = document.getElementById("type");
+selectType.addEventListener("change", ()=>{
+  let pokeTypes = filterType(selectType.value);
+
+  document.getElementById("pokedex").innerHTML=""; //las comillas borran la información
+
+  for (let i = 0; i < pokeTypes.length; i++) {
+      let pokeCards = document.createElement("div");
+      pokeCards.className = "pokeCards";
+      pokeCards.id = "pokeCards";
+
+      //variable para crear nombre pokemon
+      let pokeName = document.createElement("h3");
+      pokeName.textContent = pokeTypes[i].name;
+
+      //variable para craer imagen pokemon
+      let image = document.createElement("img");
+      image.src = pokeTypes[i].img;
+
+      //variable número pokemon
+      let pokeNumber = document.createElement("h4");
+      pokeNumber.textContent = "#" + pokeTypes[i].num;
+      pokeNumber.className = "number";
+
+      //Creando tipo de pokemon primer tipo
+      let pokeType = document.createElement("p");
+      pokeType.textContent = pokeTypes[i].type[0]; //tipo 0
+      pokeType.className = "info";
+
+      //Creando tipo de pokemon segundo tipo
+      let pokeType2 = document.createElement("p");
+      pokeType2.className = "info";
+      pokeType2.textContent = pokeTypes[i].type[1];
+
+      //Dando valor a variables
+      pokeCards.appendChild(pokeName);
+      pokeCards.appendChild(image);
+      pokeCards.appendChild(pokeNumber);
+      pokeCards.appendChild(pokeType);
+      pokeCards.appendChild(pokeType2);
+
+      //Mostrar tarjeta en HTLM section
+      document.getElementById("pokedex").appendChild(pokeCards).innerHTML;
+  }
+})
+
+
+//seleccionar ordenar por huevos
+const selectEggs = document.getElementById("eggkm");
+selectEggs.addEventListener("change", ()=>{
+  let pokeTypes = filterEggs(selectEggs.value);
+
+  document.getElementById("pokedex").innerHTML=""; //las comillas limpian la información, para que no sume
+
+  for (let i = 0; i < pokeTypes.length; i++) {
+      let pokeCards = document.createElement("div");
+      pokeCards.className = "pokeCards";
+      pokeCards.id = "pokeCards";
+
+      //variable para crear nombre pokemon
+      let pokeName = document.createElement("h3");
+      pokeName.textContent = pokeTypes[i].name;
+
+      //variable para craer imagen pokemon
+      let image = document.createElement("img");
+      image.src = pokeTypes[i].img;
+
+      //variable número pokemon
+      let pokeNumber = document.createElement("h4");
+      pokeNumber.textContent = "#" + pokeTypes[i].num;
+      pokeNumber.className = "number";
+
+      //Creando tipo de pokemon primer tipo
+      let pokeType = document.createElement("p");
+      pokeType.textContent = pokeTypes[i].type[0]; //tipo 0
+      pokeType.className = "info";
+
+      //Creando tipo de pokemon segundo tipo
+      let pokeType2 = document.createElement("p");
+      pokeType2.className = "info";
+      pokeType2.textContent = pokeTypes[i].type[1];
+
+      //Dando valor a variables
+      pokeCards.appendChild(pokeName);
+      pokeCards.appendChild(image);
+      pokeCards.appendChild(pokeNumber);
+      pokeCards.appendChild(pokeType);
+      pokeCards.appendChild(pokeType2);
+
+      //Mostrar tarjeta en HTLM section
+      document.getElementById("pokedex").appendChild(pokeCards).innerHTML;
+  }
+})
+ */
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*Mostrar Mas Detalles de la Carta
+    const pokeCard = document.querySelector(".viewCard");
+    console.log(pokeCard);
+    pokeCard.addEventListener("click", function(e) {
+      const numId = document.getElementById("pokemon");
+      //console.log(numId);
+      const numPokemon = numId;
+      //console.log(numPokemon);
+      let chosenPoke = cardPoke(allData,numPokemon);
+      //console.log(chosenPoke);
+      viewAllPokemon(chosenPoke);
+    });
+    */
+    
+    
+    /*
+    document.querySelectorAll('.elementCard').forEach(function(el){
+      el.addEventListener('click', function() {
+        const numPokemon = this.id;
+        let chosenPoke = cardPoke(allData,numPokemon);
+        console.log(chosenPoke);
+        //viewAllPokemon(chosenPoke);
+      });
+    });
+    
+    */
+    
+    /* let on= (dataSet)=>{
+      document.getElementById("overlay").style.display = "block";
+      const ovCard = document.getElementById("overlayCard");
+      let cardTemplate = "";
+      cardTemplate += `
+      <section class="tarjetaPrincipal">
+      <div class="contenedor">
+      <section class="izquierda">
+      <div class="caracteristicaPrincipal">
+      <p>${dataSet[0].num}</p>
+      <p>${dataSet[0].name}</p>
+      <img src="${dataSet[0].img}">
+      <p>Tipo: ${dataSet[0].type.join(" - ")}</p>
+      </div>
+      
+      </section>
+      
+      <section class="derecha">
+      <div class="caracteristicasSecundarias">
+      <p>Altura: ${dataSet[0].height}</p>
+      <p>Peso: ${dataSet[0].weight}</p>
+      <p>Caramelos: ${dataSet[0].candy} </p>
+      <p>Cantidad Caramelos:${dataSet[0].candy_count} </p>
+      </div>
+      <div class="debilidades">
+      <p>Debilidad: ${dataSet[0].weaknesses.join(" - ")}</p>
+      </div>
+      </section>
+      </div>
+      <section class="fondo">
+      <div class="subFondo">
+      <img src="http://www.serebii.net/pokemongo/pokemon/002.png">
+      <img src="http://www.serebii.net/pokemongo/pokemon/003.png">
+      </div>
+      
+      </section>
+      </section>`;
+      
+      ovCard.innerHTML = cardTemplate;
+      console.log(ovCard);
+    }
+    document.getElementById("pokemon1").addEventListener("click", on(allData));
+    function off() {
+      document.getElementById("overlay").style.display = "none";
+    }
+    document.getElementById("overlay").addEventListener("click", off); */
+  // © 2020 GitHub, Inc.
+  // Terms
+  // Privacy
+  // Security
+  // Status
+  // Help
+  // Contact GitHub
+  // Pricing
+  // API
+  // Training
+  // Blog
+  // About
+  
+
+
+
+
+// //Ver los Pokemones
+// const pokeDatos = pokeData.pokemon;
+// let arrData = Object.values(pokeDatos)
+// const containerPokemon = document.getElementById("containerPokemon");
+// for (let i = 0; i < arrData.length; i++) {
+//   containerPokemon.innerHTML += `<div class="cardP">
+//     <h3 class="pokeNumber">${arrData[i].num}</h3>
+//     <img src="${arrData[i].img}" alt="" class="pokeImg">
+//     <h4 class="pokeName">${arrData[i].name}</h4>
+//   </div>`;
+//   // createModal();
+// }
+
+// //seleccionar ordenar por alfabeto o numero
+// const select = document.getElementById("filterOrder");
+// select.addEventListener("change", ()=>{
+// containerPokemon.innerHTML= "";
+// arrData=Object.values(pokeDatos);
+// let condition= select.options[select.filterOrderIndex].text;
+// let orderResult=orderData(arrData, condition);
+// for(let i=0; i<orderResult.length;i++){
+//   containerPokemon.innerHTML += `<div class="cardP">
+//   <h3 class="pokeNumber">${orderResult[i].num}</h3>
+//   <img src="${orderResult[i].img}" alt="" class="pokeImg">
+//   <h4 class="pokeName">${orderResult[i].name}</h4>
+// </div>`;
+// // createModal();
+// }
+// })
+
+
+
+
+/*
+//seleccionar ordenar por tipo
+const selectType = document.getElementById("type");
+selectType.addEventListener("change", ()=>{
+  let pokeTypes = filterType(selectType.value);
+
+  document.getElementById("pokedex").innerHTML=""; //las comillas borran la información
+
+  for (let i = 0; i < pokeTypes.length; i++) {
+      let pokeCards = document.createElement("div");
+      pokeCards.className = "pokeCards";
+      pokeCards.id = "pokeCards";
+
+      //variable para crear nombre pokemon
+      let pokeName = document.createElement("h3");
+      pokeName.textContent = pokeTypes[i].name;
+
+      //variable para craer imagen pokemon
+      let image = document.createElement("img");
+      image.src = pokeTypes[i].img;
+
+      //variable número pokemon
+      let pokeNumber = document.createElement("h4");
+      pokeNumber.textContent = "#" + pokeTypes[i].num;
+      pokeNumber.className = "number";
+
+      //Creando tipo de pokemon primer tipo
+      let pokeType = document.createElement("p");
+      pokeType.textContent = pokeTypes[i].type[0]; //tipo 0
+      pokeType.className = "info";
+
+      //Creando tipo de pokemon segundo tipo
+      let pokeType2 = document.createElement("p");
+      pokeType2.className = "info";
+      pokeType2.textContent = pokeTypes[i].type[1];
+
+      //Dando valor a variables
+      pokeCards.appendChild(pokeName);
+      pokeCards.appendChild(image);
+      pokeCards.appendChild(pokeNumber);
+      pokeCards.appendChild(pokeType);
+      pokeCards.appendChild(pokeType2);
+
+      //Mostrar tarjeta en HTLM section
+      document.getElementById("pokedex").appendChild(pokeCards).innerHTML;
+  }
+})
+
+
+//seleccionar ordenar por huevos
+const selectEggs = document.getElementById("eggkm");
+selectEggs.addEventListener("change", ()=>{
+  let pokeTypes = filterEggs(selectEggs.value);
+
+  document.getElementById("pokedex").innerHTML=""; //las comillas limpian la información, para que no sume
+
+  for (let i = 0; i < pokeTypes.length; i++) {
+      let pokeCards = document.createElement("div");
+      pokeCards.className = "pokeCards";
+      pokeCards.id = "pokeCards";
+
+      //variable para crear nombre pokemon
+      let pokeName = document.createElement("h3");
+      pokeName.textContent = pokeTypes[i].name;
+
+      //variable para craer imagen pokemon
+      let image = document.createElement("img");
+      image.src = pokeTypes[i].img;
+
+      //variable número pokemon
+      let pokeNumber = document.createElement("h4");
+      pokeNumber.textContent = "#" + pokeTypes[i].num;
+      pokeNumber.className = "number";
+
+      //Creando tipo de pokemon primer tipo
+      let pokeType = document.createElement("p");
+      pokeType.textContent = pokeTypes[i].type[0]; //tipo 0
+      pokeType.className = "info";
+
+      //Creando tipo de pokemon segundo tipo
+      let pokeType2 = document.createElement("p");
+      pokeType2.className = "info";
+      pokeType2.textContent = pokeTypes[i].type[1];
+
+      //Dando valor a variables
+      pokeCards.appendChild(pokeName);
+      pokeCards.appendChild(image);
+      pokeCards.appendChild(pokeNumber);
+      pokeCards.appendChild(pokeType);
+      pokeCards.appendChild(pokeType2);
+
+      //Mostrar tarjeta en HTLM section
+      document.getElementById("pokedex").appendChild(pokeCards).innerHTML;
+  }
+})
+ */
